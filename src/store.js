efd6595eb8b886682954c415327d17c658841a7b
hazantip <hazantip@rabbi.co.il>
2018-01-17 13:12:04 +0200

    fix store.js

--- src/store.js
+++ src/store.js
@@ -1,71 +1,71 @@
-import {createStore, compose, applyMiddleware} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
 // 'routerMiddleware': the new way of storing route changes with redux middleware since rrV4.
-import { routerMiddleware } from 'react-router-redux';
+import {routerMiddleware} from 'react-router-redux';
 import rootReducer from './createReducers';
-import { apiClient } from './apiClient';
+import {apiClient} from './apiClient';
 
 export const history = createHistory();
 
 function configureStoreProd(initialState) {
-  const reactRouterMiddleware = routerMiddleware(history);
-  const isBrowser = typeof window !== 'undefined';
-  const thunkExtraArguments = {
-		'apiClient': apiClient({ initialState, isBrowser, history }),
-  };
-  const middlewares = [
-    // Add other middleware on this line...
+	const reactRouterMiddleware = routerMiddleware(history);
+	const isBrowser = typeof window !== 'undefined';
+	const thunkExtraArguments = {
+		'apiClient': apiClient({initialState, isBrowser, history}),
+	};
+	const middlewares = [
+		// Add other middleware on this line...
 
-    // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-	thunk.withExtraArgument(thunkExtraArguments),
-    reactRouterMiddleware,
-  ];
+		// thunk middleware can also accept an extra argument to be passed to each thunk action
+		// https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+		thunk.withExtraArgument(thunkExtraArguments),
+		reactRouterMiddleware,
+	];
 
-  return createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middlewares)
-    )
-  );
+	return createStore(rootReducer, initialState, compose(
+		applyMiddleware(...middlewares)
+		)
+	);
 }
 
 function configureStoreDev(initialState) {
-  const reactRouterMiddleware = routerMiddleware(history);
+	const reactRouterMiddleware = routerMiddleware(history);
 
 	const isBrowser = typeof window !== 'undefined';
 
 	const thunkExtraArguments = {
-		'apiClient': apiClient({ initialState, isBrowser, history }),
+		'apiClient': apiClient({initialState, isBrowser, history}),
 	};
 
-  const middlewares = [
-    // Add other middleware on this line...
+	const middlewares = [
+		// Add other middleware on this line...
 
-    // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
-    reduxImmutableStateInvariant(),
+		// Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
+		reduxImmutableStateInvariant(),
 
-    // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
-	thunk.withExtraArgument(thunkExtraArguments),
-	reactRouterMiddleware,
-  ];
+		// thunk middleware can also accept an extra argument to be passed to each thunk action
+		// https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+		thunk.withExtraArgument(thunkExtraArguments),
+		reactRouterMiddleware,
+	];
 
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-  const store = createStore(rootReducer, initialState, composeEnhancers(
-    applyMiddleware(...middlewares)
-    )
-  );
+	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+	const store = createStore(rootReducer, initialState, composeEnhancers(
+		applyMiddleware(...middlewares)
+		)
+	);
 
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('./', () => {
-      const nextReducer = require('./createReducers').default; // eslint-disable-line global-require
-      store.replaceReducer(nextReducer);
-    });
-  }
+	if (module.hot) {
+		// Enable Webpack hot module replacement for reducers
+		module.hot.accept('./', () => {
+			const nextReducer = require('./createReducers').default; // eslint-disable-line global-require
+			store.replaceReducer(nextReducer);
+		});
+	}
 
-  return store;
+	return store;
 }
 
 const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
